8b8dc31dea4ed1aab38edfe4208c7ada2d33f762
Israel Rdoriguez <israel355576@gmail.com>
2021-03-09 19:32:26 -0800

    Finished the location and contact sections

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
-console.log("hihi");
-
 //STOPS VIDEO WHEN MODAL IS CLOSED
 $("body").on("hidden.bs.modal", ".modal", function () {
   $("video").trigger("pause");
 });
 
 // GOOGLE MAPS
+// function initMap() {
+// Center and Zoom
 
-function initMap() {
-  // Center and Zoom
+// var options = {
+//   center: { lat: 47.6062, lng: -122.3321 },
+//   zoom: 13,
+// };
 
-  var options = {
-    center: { lat: 47.6062, lng: -122.3321 },
-    zoom: 11,
-  };
+// NEW MAP
+// map = new google.maps.Map(document.getElementById("map"), options);
 
-  // NEW MAP
-  map = new google.maps.Map(document.getElementById("map"), options);
+//MARKER
+//   const marker = new google.maps.Marker({
+//     position: { lat: 47.6205, lng: -122.3493 },
+//     map: map,
+//   });
+// }
 
-  //MARKER
-  const marker = new google.maps.Marker({
-      position:{lat: 47.6205, lng: -122.3493},
-      map: map
-  })
-}
+//SMOOTH SCROLLING
+$(".navbar a").on("click", function (e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800
+    );
+  }
+});
+
+// Changes NAVBAR COLOR WHEN SCROLLING DOWN
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+  if (scroll < 600) {
+    $(".fixed-top").css("background", "transparent");
+  } else {
+    $(".fixed-top").css("background", "black");
+  }
+});
